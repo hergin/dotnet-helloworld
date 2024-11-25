9f765e1fc49bb906a3ee9e9de253bceaca68d011
Huseyin Ergin <mersinus@gmail.com>
2024-11-25 22:35:26 +0000

    ui added

---
+++ PrimeUI/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace PrimeUI;
+
+using Spectre.Console;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        AnsiConsole.Markup("[underline red]Hello[/] World!");
+    }
+}
